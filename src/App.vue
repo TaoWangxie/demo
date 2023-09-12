b37778502c6129e4711a64c717a6f7fdaeadc99f
wangt43 <wangt43@ziroom.com>
2023-09-12 20:17:45 +0800

    youhua

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup>
-</script>
-
-<template>11111</template>
-
-<style scoped>
+<template>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
+<style>
+html,
+body {
+  position: relative;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  position: relative;
+  height: 100%;
+  overflow: hidden;
+}
 </style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">111</div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue'
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  overflow-x: hidden;
+}
+</style>
